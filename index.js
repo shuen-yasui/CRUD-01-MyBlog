ba95aef23c9a359724cb34a8d199aa60be19d322
Shuen Yasui <shuenyasui@gmail.com>
2019-06-10 19:54:18 -0700

    Added server functionality using epilogue, sequelize and sqlite3.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const Sequelize = require('sequelize');
+const epilogue = require('epilogue'), ForbiddenError = epilogue.Errors.ForbiddenError;
 const { ExpressOIDC } = require('@okta/oidc-middleware');
 
 const app = express();
@@ -34,26 +36,67 @@ app.use(session({
 app.use(oidc.router);
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  if (req.userContext.userinfo) {
-    res.send(`Hi ${req.userContext.userinfo.name}!`);
+  if (req.isAuthenticated()) {
+    res.redirect('/admin');
   } else {
     res.redirect('/home');
   }
 });
 
 app.get('/home', (req, res) => {
- res.send('<h1>Welcome!!</div><a href="/login">Login</a>');
+   res.sendFile(path.join(__dirname, './public/home.html'));
 });
 
-app.get('/admin', oidc.ensureAuthenticated(), (req, res) =>{
-  res.send('Admin page');
-})
+app.get('/admin', oidc.ensureAuthenticated(), (req, res) => {
+   res.sendFile(path.join(__dirname, './public/admin.html'));
+});
 
 app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/home');
 });
 
-app.listen(port, () => console.log(`My Blog App listening on port ${port}!`))
+const database = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite'
+});
+
+const Post = database.define('posts', {
+    title: Sequelize.STRING,
+    content: Sequelize.TEXT
+});
+
+epilogue.initialize({
+  app: app,
+  sequelize: database
+});
+
+const PostResource = epilogue.resource({
+    model: Post,
+    endpoints: ['/posts', '/posts/:id']
+});
+
+PostResource.all.auth(function (req, res, context) {
+    return new Promise(function (resolve, reject) {
+        if (!req.isAuthenticated()) {
+            res.status(401).send({ message: "Unauthorized" });
+            resolve(context.stop);
+        } else {
+            resolve(context.continue);
+        }
+    });
+});
+
+database.sync().then(() => {
+    oidc.on('ready', () => {
+        app.listen(port, () => console.log(`My Blog App listening on port ${port}!`))
+    });
+});
+
+oidc.on('error', err => {
+    // An error occurred while setting up OIDC
+    console.log("oidc error: ", err);
+});
